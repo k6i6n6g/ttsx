5a2e8c6589df18259c0ecba494fe69bc24fd7ce0
King <123456789@qq.com>
2019-01-08 20:19:55 +0800

    fastdfds保存图片

--- ttsx/models/model.go
+++ ttsx/models/model.go
@@ -3,6 +3,7 @@ package models
 import (
 	"github.com/astaxie/beego/orm"
 	_ "github.com/go-sql-driver/mysql"
+	"time"
 )
 
 //主要放表的设计
@@ -13,10 +14,72 @@ type User struct {
 	Pwd string `orm:"size(100)"`			   //密码
 	Email string							   //邮箱
 	Power int  `orm:"default(0)"` //0标识普通用户  1标识管理员用户  用户权限
-	Active int `orm:"default(0)"` //0标识未激活，1标识激活    是否激活 邮箱激活
+	Active int `orm:"default(0)"` //0标识未激活，1标识激活    是否激活
 
 	Receivers []*Receiver `orm:"reverse(many)"`
 }
+type Goods struct { //商品SPU表
+	Id 		int
+	Name 	string`orm:"size(20)"`  //商品名称
+	Detail 	string`orm:"size(200)"` //详细描述
+	GoodsSKU []*GoodsSKU `orm:"reverse(many)"`
+}
+
+type GoodsType struct{//商品类型表
+	Id int
+	Name string			//种类名称
+	Logo string			//logo
+	Image string   		//图片
+	GoodsSKU []*GoodsSKU `orm:"reverse(many)"`
+	IndexTypeGoodsBanner  []*IndexTypeGoodsBanner  `orm:"reverse(many)"`
+}
+
+type GoodsSKU struct { //商品SKU表
+	Id int
+	Goods     *Goods 	 `orm:"rel(fk)"` //商品SPU
+	GoodsType *GoodsType `orm:"rel(fk)"`  //商品所属种类
+	Name       string					 //商品名称
+	Desc       string					 //商品简介
+	Price      int						 //商品价格
+	Unite      string					 //商品单位
+	Image      string				 	 //商品图片
+	Stock      int	`orm:"default(1)"`	 //商品库存
+	Sales      int	`orm:"default(0)"`	 //商品销量
+	Status     int	 `orm:"default(1)"`	 //商品状态
+	Time       time.Time `orm:"auto_now_add"`  //添加时间
+	GoodsImage []*GoodsImage `orm:"reverse(many)"`
+	IndexGoodsBanner   []*IndexGoodsBanner `orm:"reverse(many)"`
+	IndexTypeGoodsBanner []*IndexTypeGoodsBanner  `orm:"reverse(many)"`
+}
+
+type GoodsImage struct { //商品图片表
+	Id 			int
+	Image 		string					//商品图片
+	GoodsSKU 	*GoodsSKU   `orm:"rel(fk)"` //商品SKU
+}
+type IndexGoodsBanner struct { //首页轮播商品展示表
+	Id 		  int
+	GoodsSKU  *GoodsSKU	`orm:"rel(fk)"`	//商品sku
+	Image     string					//商品图片
+	Index     int  `orm:"default(0)"`   //展示顺序
+}
+
+type IndexTypeGoodsBanner struct {//首页分类商品展示表
+	Id 				int
+	GoodsType 		*GoodsType 	`orm:"rel(fk)"`			//商品类型
+	GoodsSKU  		*GoodsSKU  	`orm:"rel(fk)"`			//商品sku
+	DisplayType 	int   		`orm:"default(1)"`		//展示类型 0代表文字，1代表图片
+	Index 			int   		`orm:"default(0)"`		//展示顺序
+}
+
+type IndexPromotionBanner struct {//首页促销商品展示表
+	Id 		int
+	Name 	string	`orm:"size(20)"`				//活动名称
+	Url 	string	`orm:"size(50)"`				//活动链接
+	Image 	string						//活动图片
+	Index 	int  `orm:"default(0)"` //展示顺序
+}
+
 
 type Receiver struct {
 	Id int
@@ -31,10 +94,11 @@ type Receiver struct {
 }
 
 
+
 func init(){
 	orm.RegisterDataBase("default","mysql","root:123456@tcp(127.0.0.1:3306)/ttsx?charset=utf8")
 	//注册表
-	orm.RegisterModel(new(User),new(Receiver))
+	orm.RegisterModel(new(User),new(Goods),new(GoodsType),new(GoodsSKU),new(GoodsImage),new(IndexGoodsBanner),new(IndexPromotionBanner),new(IndexTypeGoodsBanner),new(Receiver))
 	//跑起来
 	orm.RunSyncdb("default",false,true)
 }
